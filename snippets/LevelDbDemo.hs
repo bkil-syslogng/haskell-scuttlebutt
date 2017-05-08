91b07e52e6e9c201eec9f3e9f4e8bea1e76dfb50
Peter Hajdu <peter.hajdu@balabit.com>
2017-05-08 17:15:41 +0200

    prettier print leveldb content

--- snippets/LevelDbDemo.hs
+++ snippets/LevelDbDemo.hs
@@ -9,11 +9,12 @@ import           Control.Monad
 import           Control.Monad.IO.Class       (liftIO)
 import           Control.Monad.Trans.Resource (release)
 import           Data.ByteString.Char8        hiding (take)
+import qualified Data.ByteString.Char8        as BS
 import           Data.Default
 import           Data.Monoid
 import           Database.LevelDB
 import qualified Database.LevelDB.Streaming   as S
-import           Prelude                      hiding (putStrLn)
+import           Prelude                      hiding (putStrLn, putStr)
 
 streamDb folder =
   runResourceT $ do
@@ -22,17 +23,17 @@ streamDb folder =
                              , cacheSize= 2048
                              }
     --get db def key >>= liftIO . print
-    withIterator db def $ \iter -> dumpEntries iter
-    return ()
+    withIterator db def entries
   where
-    dumpEntries iter = liftIO $
-            S.toList (S.entrySlice iter S.AllKeys S.Asc)
-        >>= print
+    entries iter = liftIO $ (S.toList (S.entrySlice iter S.AllKeys S.Asc) :: IO [(S.Key, S.Value)])
 
 main :: IO ()
 main = do
   maybeFolder <- listToMaybe <$> getArgs
   maybe (putStrLn "usage: leveldb-reader <leveldb folder>")
-        streamDb
+        (\folder -> do
+          entries <- streamDb folder
+          mapM_ (\(key, value) -> BS.putStrLn $ BS.concat [key, " | ", value]) entries
+          )
         maybeFolder
 
