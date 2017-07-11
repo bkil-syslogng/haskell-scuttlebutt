ada8910139c7f3c60157821841db2f0159064c08
Imre Gulyas <imre.gulyas@balabit.com>
2017-07-11 16:19:13 +0200

    add multilining
    Signed-off-by: Imre Gulyas <imre.gulyas@balabit.com>

--- snippets/SsbSignatureCheck.hs
+++ snippets/SsbSignatureCheck.hs
@@ -35,7 +35,27 @@ cutSignature msg = let
  val =  init0 ++ [BS.init last0]
  in BS.init $ BS8.unlines $ val ++ ["}"]
 
-originalMessage = "{\n  \"previous\": \"%3AWRZYdDHKOmLOWvzHbwJFjx9g8hOQH/NXZkwciA63Y=.sha256\",\n  \"author\": \"@EMovhfIrFk4NihAKnRNhrfRaqIhBv1Wj8pTxJNgvCCY=.ed25519\",\n  \"sequence\": 184,\n  \"timestamp\": 1449954503740,\n  \"hash\": \"sha256\",\n  \"content\": {\n    \"type\": \"post\",\n    \"text\": \"@johnny use https://github.com/ssbc/ssb-msgs and https://github.com/ssbc/ssb-ref is probably useful too.\",\n    \"root\": \"%rf1JvoFg1pHE6TkuuMxsjBNevFck7LQvXGhkLMNlaYs=.sha256\",\n    \"branch\": \"%G37a4PUTbysiETQazyYATwLkfn/ZzigmIheAx905MLU=.sha256\",\n    \"mentions\": [\n      {\n        \"link\": \"@dnr1swLSAgf36g+FzGjNLgmytj2IIyDaYeKZ7F5GdzY=.ed25519\",\n        \"name\": \"johnny\"\n      }\n    ]\n  },\n  \"signature\": \"28GiO52GFjJnrwpKo359FamEes7JB9gTiiZaidKLL1C1NRueqGq2IAYQ1V+T2AnBgUJLRZUIyNtLTlBcx4RGAA==.sig.ed25519\"\n}"
+originalMessage = "{\n\
+                    \  \"previous\": \"%3AWRZYdDHKOmLOWvzHbwJFjx9g8hOQH/NXZkwciA63Y=.sha256\",\n\
+                    \  \"author\": \"@EMovhfIrFk4NihAKnRNhrfRaqIhBv1Wj8pTxJNgvCCY=.ed25519\",\n\
+                    \  \"sequence\": 184,\n\
+                    \  \"timestamp\": 1449954503740,\n\
+                    \  \"hash\": \"sha256\",\n\
+                    \  \"content\": {\n\
+                    \    \"type\": \"post\",\n\
+                    \    \"text\": \"@johnny use https://github.com/ssbc/ssb-msgs and https://github.com/ssbc/ssb-ref is probably useful too.\",\n\
+                    \    \"root\": \"%rf1JvoFg1pHE6TkuuMxsjBNevFck7LQvXGhkLMNlaYs=.sha256\",\n\
+                    \    \"branch\": \"%G37a4PUTbysiETQazyYATwLkfn/ZzigmIheAx905MLU=.sha256\",\n\
+                    \    \"mentions\": [\n\
+                    \      {\n\
+                    \        \"link\": \"@dnr1swLSAgf36g+FzGjNLgmytj2IIyDaYeKZ7F5GdzY=.ed25519\",\n\
+                    \        \"name\": \"johnny\"\n\
+                    \      }\n\
+                    \    ]\n\
+                    \  },\n\
+                    \  \"signature\": \"28GiO52GFjJnrwpKo359FamEes7JB9gTiiZaidKLL1C1NRueqGq2IAYQ1V+T2AnBgUJLRZUIyNtLTlBcx4RGAA==.sig.ed25519\"\n\
+                    \\
+                    \}"
 
 dropAfterChar ch str = Prelude.init $ Prelude.reverse $ Prelude.dropWhile (/= ch) $ Prelude.reverse str
 
