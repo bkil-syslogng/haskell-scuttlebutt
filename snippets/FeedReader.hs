40ce0838b929e9e12fb17001e3eba924ad7831a2
Peter Hajdu <peter.hajdu@balabit.com>
2017-08-01 17:40:26 +0200

    calculate the last messages id by feed

--- snippets/FeedReader.hs
+++ snippets/FeedReader.hs
@@ -4,12 +4,14 @@
 
 module Main where
 
+import Data.Int (Int64)
 import GHC.Generics
 import System.Environment
 import Ssb.Message
 import Data.ByteString.Lazy.Char8 as LBS hiding (foldl)
 import Data.Aeson
 import qualified Data.Map as Map
+import Prelude hiding (sequence)
 
 type Timestamp = Integer
 data Frame = Frame { key :: HashType,
@@ -25,6 +27,7 @@ parseFrames str =
 
 type FeedMap = Map.Map HashType (Message Value)
 type Feeds = Map.Map FeedLink FeedMap
+type LastIds = Map.Map FeedLink (Int64, HashType)
 
 buildFeedMap :: [Frame] -> Feeds
 buildFeedMap = foldl mergeStuff (Map.fromList [])
@@ -39,6 +42,19 @@ buildFeedMap = foldl mergeStuff (Map.fromList [])
 printFeedReport :: FeedLink -> FeedMap -> IO ()
 printFeedReport feedId feed = Prelude.putStrLn $ "feed: " ++ feedId ++ " " ++ (show $ Map.size feed)
 
+printLastIds :: FeedLink -> (Int64, HashType) -> IO ()
+printLastIds feedId (seq, mid) = Prelude.putStrLn $ "feed: " ++ feedId ++ " " ++ (show $ seq) ++ " " ++ mid
+
+calculateLastIds :: Feeds -> LastIds
+calculateLastIds = fmap calculateLastId
+  where calculateLastId :: FeedMap -> (Int64, HashType)
+        calculateLastId = Map.foldWithKey keepLast (0, "")
+
+        keepLast :: HashType -> (Message Value) -> (Int64, HashType) -> (Int64, HashType)
+        keepLast messageId message old@(lastSeq, _)
+          | lastSeq < sequence message = (sequence message, messageId)
+          | otherwise = old
+
 main :: IO ()
 main = do
   args <- getArgs
@@ -47,4 +63,7 @@ main = do
   let numberOfFeeds = Map.size feeds
   Prelude.putStrLn $ "Number of feeds: " ++ (show numberOfFeeds)
   Map.traverseWithKey printFeedReport feeds
+
+  let lastIds = calculateLastIds feeds
+  Map.traverseWithKey printLastIds lastIds
   return ()
