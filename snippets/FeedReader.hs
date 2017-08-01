f40801171838f7692d52d1e0c0b63abd1b03ea25
Peter Hajdu <peter.hajdu@balabit.com>
2017-08-01 17:04:53 +0200

    build feedmap from frames

--- snippets/FeedReader.hs
+++ snippets/FeedReader.hs
@@ -7,8 +7,9 @@ module Main where
 import GHC.Generics
 import System.Environment
 import Ssb.Message
-import Data.ByteString.Lazy.Char8 as LBS
+import Data.ByteString.Lazy.Char8 as LBS hiding (foldl)
 import Data.Aeson
+import qualified Data.Map as Map
 
 type Timestamp = Integer
 data Frame = Frame { key :: HashType,
@@ -22,8 +23,24 @@ parseFrames :: ByteString -> Either String [Frame]
 parseFrames str =
   eitherDecode str
 
+type FeedMap = Map.Map HashType (Message Value)
+type Feeds = Map.Map FeedLink FeedMap
+
+buildFeedMap :: [Frame] -> Feeds
+buildFeedMap = foldl mergeStuff (Map.fromList [])
+  where mergeStuff :: Feeds -> Frame -> Feeds
+        mergeStuff feeds frame = let feedId = author $ value frame
+                                 in Map.alter (updateFeed frame) feedId feeds
+        updateFeed :: Frame -> Maybe FeedMap -> Maybe FeedMap
+        updateFeed frame (Just feed) = Just $ Map.insert (key frame) (value frame) feed
+        updateFeed frame Nothing = let message = value frame
+                                    in Just $ Map.fromList [(key frame, message)]
+
 main :: IO ()
 main = do
   args <- getArgs
   chain <- LBS.readFile $ Prelude.head args
-  print $ parseFrames chain
+  print $ either
+    (const (Map.fromList []))
+    buildFeedMap
+    (parseFrames chain)
