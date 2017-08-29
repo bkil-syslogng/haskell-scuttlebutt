641bba098640e38e98833b39273dd47bb63032a2
Tamas Nagy <tamas.nagy@balabit.com>
2017-08-29 17:35:46 +0200

    fix(FeedReader): use Feed interface instead of FeedMap

--- snippets/FeedReader.hs
+++ snippets/FeedReader.hs
@@ -10,8 +10,10 @@ import System.Environment
 import Ssb.Message
 import Data.ByteString.Lazy.Char8 as LBS hiding (foldl)
 import Data.Aeson
+import Ssb.Feed as Feed
 import qualified Data.Map as Map
 import Prelude hiding (sequence)
+import Data.Maybe(catMaybes)
 
 type Timestamp = Integer
 data Frame = Frame { key :: HashType,
@@ -25,45 +27,37 @@ parseFrames :: ByteString -> Either String [Frame]
 parseFrames str =
   eitherDecode str
 
-type FeedMap = Map.Map HashType (Message Value)
-type Feeds = Map.Map FeedLink FeedMap
-type LastIds = Map.Map FeedLink (Int64, HashType)
+--type FeedMap = Map.Map HashType (Message Value)
+type Feeds = Map.Map FeedLink (Feed Value)
+type LastIds = [(Int64, HashType)]
 
 buildFeedMap :: [Frame] -> Feeds
 buildFeedMap = foldl mergeStuff (Map.fromList [])
   where mergeStuff :: Feeds -> Frame -> Feeds
         mergeStuff feeds frame = let feedId = author $ value frame
                                  in Map.alter (updateFeed frame) feedId feeds
-        updateFeed :: Frame -> Maybe FeedMap -> Maybe FeedMap
-        updateFeed frame (Just feed) = Just $ Map.insert (key frame) (value frame) feed
+        updateFeed :: Frame -> Maybe (Feed Value) -> Maybe (Feed Value)
+        updateFeed frame (Just feed) = Just $ add feed (value frame)
         updateFeed frame Nothing = let message = value frame
-                                    in Just $ Map.fromList [(key frame, message)]
+                                    in Just $ Feed.empty $ author message
 
-printFeedReport :: FeedLink -> FeedMap -> IO ()
-printFeedReport feedId feed = Prelude.putStrLn $ "feed: " ++ feedId ++ " " ++ (show $ Map.size feed)
+printFeedReport :: FeedLink -> Feed Value -> IO ()
+printFeedReport feedId feed = Prelude.putStrLn $ "feed: " ++ feedId ++ " " ++ (show $ Feed.length feed)
 
 printLastIds :: FeedLink -> (Int64, HashType) -> IO ()
 printLastIds feedId (seq, mid) = Prelude.putStrLn $ "feed: " ++ feedId ++ " " ++ (show $ seq) ++ " " ++ mid
 
 calculateLastIds :: Feeds -> LastIds
-calculateLastIds = fmap calculateLastId
-  where calculateLastId :: FeedMap -> (Int64, HashType)
-        calculateLastId = Map.foldWithKey keepLast (0, "")
-
-        keepLast :: HashType -> (Message Value) -> (Int64, HashType) -> (Int64, HashType)
-        keepLast messageId message old@(lastSeq, _)
-          | lastSeq < sequence message = (sequence message, messageId)
-          | otherwise = old
+calculateLastIds feeds = catMaybes $ (snd <$> Map.toList maybeIds) where
+  maybeIds = fmap getLastMessage feeds
+  getLastMessage feed = fmap (\msg -> (sequence msg, author msg)) (Feed.lastMessage feed)
 
 main :: IO ()
 main = do
   args <- getArgs
   chain <- LBS.readFile $ Prelude.head args
   let feeds = either (const (Map.fromList [])) buildFeedMap (parseFrames chain)
-  let numberOfFeeds = Map.size feeds
-  Prelude.putStrLn $ "Number of feeds: " ++ (show numberOfFeeds)
-  Map.traverseWithKey printFeedReport feeds
 
   let lastIds = calculateLastIds feeds
-  Map.traverseWithKey printLastIds lastIds
+  mapM_ print lastIds
   return ()
